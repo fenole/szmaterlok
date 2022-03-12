f2a9f324d88d6798562b2d3d31b06d0c411a7e00
thinkofher <mail@bdudek.xyz>
2022-03-12 14:49:57 +0100

    feat: Setup web server with static files.

--- cmd/szmaterlok/main.go
+++ cmd/szmaterlok/main.go
@@ -1,9 +1,65 @@
 package main
 
-import "log"
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/fenole/szmaterlok/web"
+)
 
 func run() error {
-	return nil
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Handle("/*", http.FileServer(http.FS(web.Assets)))
+
+	c := make(chan os.Signal, 1)
+	errc := make(chan error, 1)
+
+	wait := time.Second * 15
+	srv := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: r,
+		// TODO(thinkofher): Come back later to setup timeouts.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		log.Println("Listening at 0.0.0.0:8080...")
+		if err := srv.ListenAndServe(); err != nil {
+			errc <- err
+		}
+	}()
+
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal or error from server.
+	select {
+	case <-c:
+		ctx, cancel := context.WithTimeout(context.Background(), wait)
+		defer cancel()
+		// Doesn't block if no connections, but will otherwise wait
+		// until the timeout deadline.
+		srv.Shutdown(ctx)
+		// Optionally, you could run srv.Shutdown in a goroutine and block on
+		// <-ctx.Done() if your application should wait for other services
+		// to finalize based on context cancellation.
+		log.Println("shutting down")
+		return nil
+	case err := <-errc:
+		return err
+	}
 }
 
 func main() {
--- web/web.go
+++ web/web.go
@@ -1,3 +1,11 @@
 // Package web contains static web assets such as html files,
 // js scripts and css style sheets.
 package web
+
+import "embed"
+
+// Assets is a filesystem with static files for szmaterlok
+// web server embedded into binary.
+//
+//go:embed assets index.html
+var Assets embed.FS
