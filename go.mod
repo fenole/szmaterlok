a122c8099f86ae5e635d4f15c87d5b9908a8afc8
thinkofher <mail@bdudek.xyz>
2022-03-13 20:56:54 +0100

    feat: Configure basic sse layer for UI.

--- go.mod
+++ go.mod
@@ -3,3 +3,6 @@ module github.com/fenole/szmaterlok
 go 1.17
 
 require github.com/go-chi/chi/v5 v5.0.7
+
+// Tests
+require github.com/matryer/is v1.4.0
--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,3 +1,41 @@
-function HelloWorld() {
-  console.log("Hello World!");
+const eventStreamResource = "/stream";
+const sseTypes = ["test"];
+
+function setupSSE() {
+  let evtSource = new EventSource(eventStreamResource, {
+    withCredentials: true,
+  });
+
+  const handleEvent = (event) => {
+    document.dispatchEvent(
+      new CustomEvent("sse:" + event.type, {
+        bubbles: true,
+        detail: {
+          data: JSON.parse(event.data),
+        },
+      }),
+    );
+  };
+
+  sseTypes.forEach((eventType) => {
+    evtSource.addEventListener(eventType, handleEvent);
+  });
+
+  return evtSource;
 }
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("eventCounter", () => ({
+    count: 0,
+    message: "",
+    lastEventSendAt: 0,
+
+    handleMessage(event) {
+      this.message = event.detail.data.msg;
+      this.lastEventSendAt = event.detail.data.sendAt;
+      this.count++;
+    },
+  }));
+
+  setupSSE();
+});
