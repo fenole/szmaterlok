6c80c529fe2d08d7e71cbd2d2f5147088451e607
thinkofher <mail@bdudek.xyz>
2022-03-23 00:34:47 +0100

    feat: Implement cache for tokenizer.

---
+++ service/tokenizer.go
@@ -0,0 +1,175 @@
+package service
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"sync"
+	"time"
+
+	"filippo.io/age"
+	"github.com/sirupsen/logrus"
+)
+
+// SessionAgeTokenizer encodes and decodes session state token.
+type SessionAgeTokenizer struct {
+	recipient age.Recipient
+	identity  age.Identity
+	base64    *base64.Encoding
+}
+
+// NewSessionAgeTokenizer returns SessionAgeTokenizer which encrypts
+// and decrypts tokens with given secret. Make sure secret is
+// long enough and has high entropy.
+func NewSessionAgeTokenizer(secret string) (*SessionAgeTokenizer, error) {
+	r, err := age.NewScryptRecipient(secret)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new scrypt recipient: %w", err)
+	}
+
+	i, err := age.NewScryptIdentity(secret)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new scrypt identity: %w", err)
+	}
+
+	return &SessionAgeTokenizer{
+		recipient: r,
+		identity:  i,
+		base64:    base64.URLEncoding,
+	}, nil
+}
+
+// TokenEncode encodes given session state into encrypted and base64 encoded
+// token string, which can be used to safely store session state in users
+// browser.
+func (st *SessionAgeTokenizer) TokenEncode(state SessionState) (string, error) {
+	buff := &bytes.Buffer{}
+
+	wc, err := age.Encrypt(buff, st.recipient)
+	if err != nil {
+		return "", fmt.Errorf("failed to create encrypted writer: %w", err)
+	}
+
+	if err := json.NewEncoder(wc).Encode(state); err != nil {
+		return "", fmt.Errorf("failed to encode state into json: %w", err)
+	}
+
+	if err := wc.Close(); err != nil {
+		return "", fmt.Errorf("failed to encrypt session state: %w", err)
+	}
+
+	return st.base64.EncodeToString(buff.Bytes()), nil
+}
+
+// TokenDecode decodes given base64 encoded and encrypted token into
+// SessionState.
+func (st *SessionAgeTokenizer) TokenDecode(token string) (*SessionState, error) {
+	b, err := st.base64.DecodeString(token)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode token from base64: %w", err)
+	}
+
+	buff := bytes.NewBuffer(b)
+
+	src, err := age.Decrypt(buff, st.identity)
+	if err != nil {
+		return nil, fmt.Errorf("failed to created encrypted reader: %w", err)
+	}
+
+	out := &bytes.Buffer{}
+
+	if _, err := io.Copy(out, src); err != nil {
+		return nil, fmt.Errorf("failed to read encrypted token: %w", err)
+	}
+
+	res := &SessionState{}
+	if err := json.NewDecoder(out).Decode(res); err != nil {
+		return nil, fmt.Errorf("failed to decode session state to json: %w", err)
+	}
+
+	return res, nil
+}
+
+type sessionTokenizerCacheEntry struct {
+	value SessionState
+	timer *time.Timer
+}
+
+// SessionTokenizerCache wraps SessionTokenizer interface and extends it
+// with concurrent-safe in-memory cache storage.
+type SessionTokenizerCache struct {
+	wrapped SessionTokenizer
+	timeout time.Duration
+	log     *logrus.Logger
+	mtx     *sync.RWMutex
+	cache   map[string]sessionTokenizerCacheEntry
+}
+
+// SessionTokenizerCacheBuilder holds build arguments for SessionTokenizerCache.
+type SessionTokenizerCacheBuilder struct {
+	Wrapped SessionTokenizer
+	Timeout time.Duration
+	Logger  *logrus.Logger
+}
+
+// NewSessionTokenizerCache is default and safe constructor for SessionTokenizerCache.
+func NewSessionTokenizerCache(b SessionTokenizerCacheBuilder) *SessionTokenizerCache {
+	return &SessionTokenizerCache{
+		wrapped: b.Wrapped,
+		timeout: b.Timeout,
+		log:     b.Logger,
+		mtx:     &sync.RWMutex{},
+		cache:   make(map[string]sessionTokenizerCacheEntry),
+	}
+}
+
+// TokenEncode returns tokenized string which represents session state and
+// can be decoded with the same interface implementation.
+func (c *SessionTokenizerCache) TokenEncode(state SessionState) (string, error) {
+	return c.wrapped.TokenEncode(state)
+}
+
+// TokenDecode decodes given string token into valid session state.
+func (c *SessionTokenizerCache) TokenDecode(token string) (*SessionState, error) {
+	// Read cache entry from map. Wrap it with read lock.
+	c.mtx.RLock()
+	entry, ok := c.cache[token]
+	c.mtx.RUnlock()
+	if !ok {
+		// There is no entry in cache. Decode token manually.
+		res, err := c.wrapped.TokenDecode(token)
+		if err != nil {
+			return nil, err
+		}
+
+		// Begin write transaction.
+		c.mtx.Lock()
+
+		// Add new cache entry for given token.
+		c.cache[token] = sessionTokenizerCacheEntry{
+			value: *res,
+
+			// Fire garbage collection for given token after cache timeout.
+			timer: time.AfterFunc(c.timeout, func() {
+				s := *res
+				c.mtx.Lock()
+				defer c.mtx.Unlock()
+				delete(c.cache, token)
+				c.log.WithFields(logrus.Fields{
+					"userID":   s.ID,
+					"nickname": s.Nickname,
+				}).Debug("Garbage collection of tokenizer cache.")
+			}),
+		}
+
+		// End write transaction.
+		c.mtx.Unlock()
+
+		return res, nil
+	}
+
+	entry.timer.Reset(c.timeout)
+	return &entry.value, nil
+}
