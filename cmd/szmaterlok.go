5866f0813bd455a840b69f97591e3b07589b7266
thinkofher <mail@bdudek.xyz>
2022-04-19 03:08:38 +0200

    feat: Implement last messages buffer.
    Implement thread-safe last messages buffer which
    is synchronized with regular source of messages.

--- cmd/szmaterlok.go
+++ cmd/szmaterlok.go
@@ -46,6 +46,7 @@ func run(ctx context.Context) error {
 	stateOnlineUsers := service.NewStateOnlineUsers()
 
 	messageHandler := service.NewBridgeMessageHandler(log)
+	lastMessagesBuffer := service.NewLastMessagesBuffer(10, log)
 
 	eventRouter := service.NewBridgeEventRouter()
 	eventRouter.Hook(service.BridgeMessageSent, messageHandler)
@@ -53,6 +54,7 @@ func run(ctx context.Context) error {
 	eventRouter.Hook(service.BridgeUserLeft, messageHandler)
 	eventRouter.Hook(service.BridgeUserJoin, service.StateUserJoinHook(log, stateOnlineUsers))
 	eventRouter.Hook(service.BridgeUserLeft, service.StateUserLeftHook(log, stateOnlineUsers))
+	eventRouter.Hook(service.BridgeMessageSent, lastMessagesBuffer)
 
 	bridge := service.NewBridge(ctx, service.BridgeBuilder{
 		Handler: eventRouter,
@@ -70,9 +72,13 @@ func run(ctx context.Context) error {
 		},
 		Bridge:            bridge,
 		AllChatUsersStore: stateOnlineUsers,
-		MessageNotifier:   messageHandler,
-		IDGenerator:       service.IDGeneratorFunc(uuid.NewString),
-		Clock:             clock,
+		MessageNotifier: &service.MessageNotifierWithBuffer{
+			Notifier: messageHandler,
+			Buffer:   lastMessagesBuffer,
+			Logger:   log,
+		},
+		IDGenerator: service.IDGeneratorFunc(uuid.NewString),
+		Clock:       clock,
 	})
 
 	c := make(chan os.Signal, 1)
